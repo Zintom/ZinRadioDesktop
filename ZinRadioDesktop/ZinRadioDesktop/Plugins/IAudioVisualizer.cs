ba3f531f6a5d6f0e41cb161b5b70e5729dd282e2
Zintom <tzsinkoapps@gmail.com>
2021-06-16 18:43:08 +0100

    Plugin Bits
    Getting ready for plugin implementations.

---
+++ ZinRadioDesktop/ZinRadioDesktop/Plugins/IAudioVisualizer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace ZinRadioDesktop.Plugins
+{
+    /// <summary>
+    /// The plugin interface for displaying visualisations that represent the currently playing audio.
+    /// </summary>
+    public interface IAudioVisualizer
+    {
+        /// <returns>The information regarding this plugin.</returns>
+        PluginInfo Load();
+
+        /// <summary>
+        /// Draws a frame which is a visualization of the given <paramref name="samples"/>.
+        /// </summary>
+        /// <param name="samples"></param>
+        /// <returns>A <see cref="Bitmap"/> which represents the <paramref name="samples"/> in some visual form.</returns>
+        Bitmap DrawFrame(byte[] samples, int canvasWidth, int canvasHeight);
+    }
+
+    public static class VisualizerPluginLoader
+    {
+        private static readonly string _pluginDirectory = AppDomain.CurrentDomain.BaseDirectory + "\\Plugins";
+
+        /// <summary>
+        /// Gets all available <see cref="IAudioVisualizer"/>'s available in the <b>Plugins</b> directory.
+        /// </summary>
+        public static IReadOnlyList<IAudioVisualizer> GetAvailableAudioVisualizers()
+        {
+            List<IAudioVisualizer> plugins = new();
+
+            foreach (string file in Directory.GetFiles(_pluginDirectory))
+            {
+                IAudioVisualizer? plugin = PluginBase.CreateTypeFromDll<IAudioVisualizer>(file);
+                if (plugin != null)
+                {
+                    plugins.Add(plugin);
+                }
+            }
+
+            return plugins;
+        }
+    }
+}
---
+++ ZinRadioDesktop/ZinRadioDesktop/Plugins/PluginBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace ZinRadioDesktop.Plugins
+{
+    public class PluginInfo
+    {
+        public string Name { get; init; }
+
+        public PluginInfo(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public static class PluginBase
+    {
+        /// <summary>
+        /// Locates the first <typeparamref name="T"/> in the given <paramref name="dllFile"/> assembly and instantiates it.
+        /// </summary>
+        /// <param name="dllFile"></param>
+        /// <returns>An instantiation of the first located <typeparamref name="T"/>, or <see langword="null"/> if no types implement the interface in the assembly.</returns>
+        public static T? CreateTypeFromDll<T>(string dllFile) where T : class
+        {
+            // Search through all the types in the assembly.
+            foreach (Type type in Assembly.LoadFrom(dllFile).GetTypes())
+            {
+                // If the type matches our target type, try to instantiate it.
+                if (type is T)
+                {
+                    return (T?)Activator.CreateInstance(type);
+                }
+            }
+
+            return null;
+        }
+    }
+}

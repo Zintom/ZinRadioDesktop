b922a1f07789b2f3e6a094600eff23f0d799af9a
Zintom <tzsinkoapps@gmail.com>
2021-06-27 17:06:17 +0100

    Create Extensions.cs
    SafeInvokeIfRequired extension is a quick an easy way to invoke a delegate safely.

---
+++ ZinRadioDesktop/ZinRadioDesktop/Others/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace ZinRadioDesktop
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Invokes the given <paramref name="action"/> on the UI thread of the <paramref name="control"/> unless
+        /// we are already executing on the UI thread of the control.
+        /// </summary>
+        public static void SafeInvokeIfRequired(this ISynchronizeInvoke control, MethodInvoker action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action, null);
+            }
+            else
+            {
+                action.Invoke();
+            }
+        }
+    }
+}
